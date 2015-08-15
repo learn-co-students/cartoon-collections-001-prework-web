fa9ca06ebc526984a82b4e7065937714980d0de7
patrickhsanders <patrickhsanders@gmail.com>
2015-08-15 16:00:27 -0400

    give me the cheddar, earth, wind and fire

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+	dwarves.each_with_index do |dwarf, index|
+		puts "#{index + 1}. #{dwarf}"
+	end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+	upcalls = []
+	planeteer_calls.collect do |call|
+		upcalls << call.capitalize + "!"
+	end
+	return upcalls
 end
 
-def long_planteer_calls# code an argument here
-  # Your code here
+def long_planteer_calls(planeteer_calls)
+	return planeteer_calls.any? {|call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(snacks)
+  	cheese_types = ["cheddar", "gouda", "camembert"]
+	return snacks.find do |snack|
+		cheese_types.include?(snack)
+	end
 end
+
+
+#puts find_the_cheese( ["earth", "wind", "cheddar", "fire", "water", "heart"])
