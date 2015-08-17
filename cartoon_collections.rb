b51a036b878cdfeb8b3ba4792768a162592f7357
james-marshall314 <james.marshall314@gmail.com>
2015-08-17 16:21:21 -0400

    passing

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect do |item|
+    item.capitalize << "!"
+  end
 end
 
-def long_planteer_calls# code an argument here
+def long_planteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? do |call|
+    call.size > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    cheese_types.include? item
+  end
 end
