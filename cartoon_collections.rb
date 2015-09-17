9178c7ca7604c2718c012dedcfb6e7f85ba4b206
skipsuva <skip.suva@gmail.com>
2015-09-17 19:26:18 -0400

    Cartooney

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |name, index|
+    print "#{index +1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |x|
+    x.capitalize! << "!"
+  end
 end
 
-def long_planteer_calls# code an argument here
-  # Your code here
+
+def long_planteer_calls(calls)
+  calls.each do |x|
+    if x.length > 4
+      return true
+    else
+      return false
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.find do |x|
+    cheese_types.include?(x)
+  end
 end
