ac131560ae00dfc43c94a7685df920117d67e505
shmulim <shmuel.markel@gmail.com>
2015-11-19 14:42:31 -0800

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.each.with_index do |dwarf, idx|
+    puts "#{idx+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map { |el| el.capitalize << "!"}
 end
 
-def long_planteer_calls# code an argument here
-  # Your code here
+def long_planteer_calls(planeteer_calls)
+  planeteer_calls.any? { |calls| calls.length > 4 }
 end
 
 def find_the_cheese# code an argument here
