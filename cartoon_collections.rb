9cff776cd8c11b62dfe1bda756eb3c8463d3e0c2
opusmagnum77 <daniel.adeyanju@gmail.com>
2015-12-09 13:49:36 +0000

    Do stuff w collections

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each_with_index do |dwarve, index|
+      puts "#{index+1}. #{dwarve}"
+end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet calls
   # Your code here
+  calls.collect do |call|
+   call=call.capitalize
+  call<<"!"
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  calls.any? do |call|
+    call.size > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese arr
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  #arr.include? cheese_types
+  intersection = (cheese_types & arr).join
+  return nil if intersection.size==0
+  return intersection if intersection.size > 0
 end
