95eb33f85062e77bd69778c7ce0a8ddad7f2e182
jonalexander <jonathandalexander@gmail.com>
2016-02-20 11:15:53 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,50 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+# dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+	dwarves.each_with_index do |item, index|		
+		puts "#{index + 1}. #{item}"
+	end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+	# .capitalize will convert str's first char to UC
+
+	planeteer_calls.collect do |x|
+		x.capitalize + "!"
+	end
+end
+# planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+# => ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+
+def long_planeteer_calls(long_planeteer_calls)
+	long_planeteer_calls.each do |x|
+		# iterate through each call & check the .length 
+		if x.length > 4
+			return true
+		else
+			return false
+		end
+	end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def find_the_cheese(array)
+ cheese_types = ["cheddar", "gouda", "camembert"]
+ # snacks = ["crackers", "gouda", "thyme"]
+ # soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+answer = array.include?("cheddar" || "gouda" || "camembert")
+
+if answer == true
+	#iterate thru array and check each val against cheese_types. 
+	#if there is a match, return value otherwise return nil
+		array.each do |x|
+		#does x == one of the cheeses? if so return x
+			if x === ("cheddar" || "gouda" || "camembert")
+				return x
+				break
+			else
+			end
+		end
+	else
+	return nil
+	end
 end
