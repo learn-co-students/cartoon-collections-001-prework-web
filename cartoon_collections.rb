305b5aca366f5a6f9ad383f134003e5341b90f8a
Fidel Severino <fidelscodes@gmail.com>
2016-04-29 06:06:16 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -37,3 +37,12 @@ def find_the_cheese(array)
     end
   end
 end
+
+# Learn.co solution for #find_the_cheese
+# def find_the_cheese(potentially_cheesy_items)
+#   cheeses = %w(gouda cheddar camembert)
+
+#   potentially_cheesy_items.find do |maybe_cheese|
+#     cheeses.include?(maybe_cheese)
+#   end
+# end
