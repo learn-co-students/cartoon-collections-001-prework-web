ed52e978fc64291f8fbfe864e49100efb6a4ff1a
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-05-02 18:53:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arr)# code an argument here
   # Your code here
+  arr.each_with_index { |x,i|  puts "/#{i+1}.*#{x}/"   }
+  arr2 = arr.reverse
+  arr2.each_with_index { |x,i|  puts "/#{i+1}.*#{x}/"   }
+  arr.each_with_index { |x| puts x }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(ar)# code an argument here
   # Your code here
+  ar.map!{ |x| x.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arr)# code an argument here
   # Your code here
+  arr.each do |x|
+    if (x.length > 4 )
+      return true
+    end
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |x|
+    if ( arr.include?(x) )
+      return x
+    end
+  end
+    return nil
 end
+
