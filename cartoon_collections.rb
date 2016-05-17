2969fbee260a4af92c2aef464172ad76e65fdacc
Kellye Greene <kellye@gust.com>
2016-05-17 18:00:20 -0400

    WIP: 1st test passing

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1).collect do |dwarf, num|
+    puts "#{num}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
