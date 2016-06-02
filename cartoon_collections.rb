cabd8cb4609a22daf05f4a46fa37f257c19c22d9
Corneezius <cfr2116@columbia.edu>
2016-06-02 11:33:10 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
-    array.each_with_index { | dwarf, index| puts "#{index + 1}. #{dwarf}" }
-end
+   array.each_with_index { | dwarf, index| p "#{index + 1}. #{dwarf}" }
+  end
 
 def summon_captain_planet(array)# code an argument here
   array.collect { |planeteer| planeteer.capitalize << "!" }
@@ -9,10 +9,14 @@ def summon_captain_planet(array)# code an argument here
 
 def long_planeteer_calls(array)
     array.any? { |word| word.length > 4}
-end
+  end
 
 
 def find_the_cheese(array)
  cheese_types = ["cheddar", "gouda", "camembert"]
  array.find { |ingredients| cheese_types.include?(ingredients)}
   end
+
+
+
+
