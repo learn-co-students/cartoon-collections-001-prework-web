66e2df3ffd08cd77cd0d7f97fa76532b6e3ad2bf
abidiaz <abidiaz@users.noreply.github.com>
2016-06-06 14:44:56 -0400

    Update cartoon_collections.rb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves(array)# code an argument here
+#To print a numbered list of dwarf names
+def roll_call_dwarves(array) 
   array.each_with_index do |dwarf, index|
     puts "#{index+1}. #{dwarf}"
   end
 end
 
+#To capitalize and add exclamation points to calls
 def summon_captain_planet(array)
   calls_array = []
   calls_array = array.collect {|calls|
@@ -12,6 +14,7 @@ def summon_captain_planet(array)
   calls_array
 end
 
+#To find if any long calls are included in the array
 def long_planeteer_calls(array)
   long_calls = array.select {|calls| calls.size > 4}
   if long_calls.size > 0
@@ -21,6 +24,7 @@ def long_planeteer_calls(array)
   end
 end
 
+#To find any elements in the user's array that are the same as any elements in the cheese array  
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
