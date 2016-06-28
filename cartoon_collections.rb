1e7c29456dcad80392d10307f4ff973a131577f8
Derick Quartey <niikwartey1@gmail.com>
2016-06-28 18:51:47 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,7 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.each do |ingredient|
-    if cheese_types.include?(ingredient)
-      return ingredient
-    end
+  ingredients.find do |ingredient|
+    cheese_types.include?(ingredient)
   end
-  return nil
 end
