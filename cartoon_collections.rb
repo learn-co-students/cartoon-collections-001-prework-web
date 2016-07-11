f7e164770d057974485e87ef1062576381b19397
Dallin Wardrop <dallinwardrop12@gmail.com>
2016-07-11 23:54:50 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (names)
+  names.each do |names|
+    puts ""
 end
 
 def summon_captain_planet# code an argument here
