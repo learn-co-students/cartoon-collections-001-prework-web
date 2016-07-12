646eae2283fb2d03058b1af9d3ff56d5f8c1eb0e
Dallin Wardrop <dallinwardrop12@gmail.com>
2016-07-12 16:09:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
-def long_planteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| call.length > 4 }
+def long_planeteer_calls(array)
+  array.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(potentially_cheesy_items)
