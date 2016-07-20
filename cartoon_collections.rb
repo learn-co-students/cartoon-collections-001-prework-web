76e9fdc1690e75c27695f128c631f1f84bc921f4
Keo  Bun <keonilrath@gmail.com>
2016-07-20 02:02:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,22 @@
+#def roll_call_dwarves(dwarves)
+#  dwarves.each_with_index {|dwarf, number| puts "#{number + 1}. #{dwarf}"}
+#end
+
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|dwarf, number| puts "#{number + 1}. #{dwarf}"}
+  count = 0
+  dwarves.each {|dwarf| puts (count += 1).to_s << ". #{dwarf}"}
 end
 
+#def summon_captain_planet(elements)
+#  elements.map {|element| element.capitalize << "!" }
+#end
+
+#def summon_captain_planet(elements)
+#  elements.collect {|element| element.capitalize << "!"}
+#end
 
 def summon_captain_planet(elements)
-  elements.map {|element| element.capitalize << "!" }
+  elements.each {|element| element.capitalize! <<  "!"}
 end
 
 # def long_planeteer_calls(calls)
