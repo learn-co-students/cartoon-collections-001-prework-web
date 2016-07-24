4a25067ac1fc75dff946349f81b50821f7fe472c
dwyn-nyc <dwyn.nyc@gmail.com>
2016-07-24 18:36:02 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arg)
+  arg.each.with_index(1) do |value, index|
+    puts "#{index}. #{value}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  var = veggies
+  var.map! do |n|
+    n.capitalize + "!"
+  end
+  return var
+end
+#
+def long_planeteer_calls(var)
+  var.any? { |n| n.length > 4 }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(var)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  var.find do |n|
+    cheese_types.include?(n)
+  end
 end
