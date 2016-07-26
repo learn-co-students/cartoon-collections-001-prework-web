cc94a89ed15d061e437f4c06bd06d86f16b62e20
Elias Miller <emiller783@gmail.com>
2016-07-26 01:30:26 +0000

    finished cartoons

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, number|
+    puts "#{number + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+    veggies.collect {|name| name.capitalize+ "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  if
+    calls.find {|words| words.length > 4}
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(type)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+type.find do |words|
+  cheese_types.find { |cheese| cheese == words}
+  end
 end
